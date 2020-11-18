03307d3be7e9405e1c0dbbbfb5d5604a9842c429
FoxLib <hplus.foxtail@gmail.com>
2020-11-18 21:47:13 +0300

    Небольшие правки

--- asm/02_bublesort/bsort.asm
+++ asm/02_bublesort/bsort.asm
@@ -17,72 +17,27 @@
         stosb
         loop    @b
 
-        ; Начать сортировку 64k
-        push    0
-        push    63999
-        call    qsort
+; ----------------------------------------------------------------------
+
+        ; for (i = 0; i < 64000; i++)
+        ; for (j = i + 1; j < 64000; j++)
+
+        xor     si, si
+        mov     bx, 64000
+L2:     lea     di, [si + 1]
+L1:     mov     al, [si]
+        cmp     al, [di]
+        jb      @f
+        mov     ah, [si]
+        xchg    ah, [di]
+        xchg    ah, [si]
+@@:     inc     di
+        cmp     di, bx
+        jne     L1
+        inc     si
+        cmp     si, bx
+        jne     L2
 
         xor     ax, ax
         int     16h
         int     20h
-
-; ----------------------------------------------------------------------
-qsort:  push    bp
-        mov     bp, sp
-
-        ; Поиск центра pivot
-        mov     si, [bp + 6] ; a = l
-        mov     di, [bp + 4] ; b = r
-        mov     bx, si
-        shr     bx, 1
-        mov     ax, di
-        shr     ax, 1
-        add     bx, ax       ; BX=(a+b)/2
-        mov     al, [bx]     ; AL=pivot
-
-; --------------------------------------------------------------
-wend:   ; while arr[si] < pivot: si++
-@@:     cmp     [si], al
-        inc     si
-        jb      @b
-        dec     si
-
-        ; while pivot < arr[di]: di--
-@@:     cmp     al, [di]
-        dec     di
-        jb      @b
-        inc     di
-
-        ; if a <= b: swap arr[a], arr[b]; a++; b--
-        cmp     si, di
-        jnbe    @f
-
-        mov     ah, [si]
-        xchg    ah, [di]
-        xchg    ah, [si]
-        inc     si
-        dec     di
-
-@@:     ; loop while a <= b
-        cmp     si, di
-        jbe     wend
-; --------------------------------------------------------------
-
-        ; if l < b: qsort l, b
-        cmp     [bp + 6], di
-        jnb     @f
-        push    word [bp + 6]
-        push    di
-        call    qsort
-
-@@:     ; if a < r: qsort a, r
-        cmp     si, [bp + 4]
-        jnb     @f
-        push    si
-        push    word [bp + 4]
-        call    qsort
-
-        ; Выход из процедуры
-@@:     mov     sp, bp
-        pop     bp
-        ret     4
