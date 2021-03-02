93243bdf9f4b22a6afecf04fbbea726b9f5499c5
FoxLib <hplus.foxtail@gmail.com>
2021-03-02 14:46:32 +0300

    Удалил кое-что

--- games/README.md
+++
@@ -1,2 +0,0 @@
-# MiniGames
-Мини-игры для Youtube
--- misc/start/opengl/make.sh
+++
@@ -1,8 +0,0 @@
-# ln -s .. lib
-# Компиляция и линковка в `main`
-if (g++ `sdl-config --cflags --libs` -I../lib main.c -lSDL -lglut -lGL -lGLU -Wall -o main)
-then
-
-    echo "OK"
-    ./main
-fi
