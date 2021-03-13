6721188806fff3e4fd74b1cfb95d993d02c16c03
FoxLib <hplus.foxtail@gmail.com>
2021-03-13 16:32:03 +0300

    Drawingowat

--- asm/05_triangle/main.asm
+++ asm/05_triangle/main.asm
@@ -28,54 +28,134 @@ macro   subvec  a, b, c {
         sub     ax, [si+c]
         mov     [bp-a], ax
 }
+macro   increm  a, b {
+        movsx   eax, word [b]
+        add     [a], eax
+}
 ; ----------------------------------------------------------------------
 
+        mov     ax, 0x0013
+        int     10h
+
         ; Создание треугольника
+        push    0xA000
+        pop     es
+
         mov     si, data_tri
         mov     di, data_com
         call    create_parameter_tri
-brk
-        ; Расчет координат
-
-        movzx   eax, word [di]
-        mul     dword [.dx]
-        xchg    eax, ebx
-        movzx   eax, word [di+2]
-        mul     dword [.dy]
-        add     ebx, eax
-        movzx   eax, word [di+4]
-        mul     dword [.dz]
-        add     eax, ebx
+        call    draw_screen
 
         int     20h
 
-.dx     dd      -160
-.dy     dd      100
-.dz     dd      100
-
 ; ----------------------------------------------------------------------
-calculate_uvd:
+; Рисование треугольника
+; ----------------------------------------------------------------------
 
-        ; u = dx*A1 + dy*A2 + dz*A3
-        ; v = dx*B1 + dy*B2 + dz*B3
-        ; D = dx*C1 + dy*C2 + dz*C3
+draw_screen:
 
+        xor     di, di
+        mov     [.cursor], word 0
+        mov     [calcuvd.dy], dword 100
+
+        ; for (y = 0; y < 200; y++)
+        mov     cx, 200
+.L2:    push    cx
+
+        ; Расчет координат
+        mov     di, data_com
+        call    calcuvd
+        mov     cx, 320
+
+        ; for (x = 0; x < 320; x++)
+.L1:    mov     eax, [calcuvd.u]
+        mov     ebx, [calcuvd.v]
+        mov     edx, [calcuvd.D]
+        mov     [.cl], byte 0
+
+        ; if (u + v < D && u >= 0 && v >= 0) then
+        sub     edx, eax
+        sbb     edx, ebx
+        js      @f
+        or      eax, ebx
+        js      @f
+
+        ; (BL) u = 16*(u / D)
+        ; --------------------------------------
+        mov     eax, [calcuvd.v]
+        mov     ebx, [calcuvd.D]
+        shr     ebx, 4
+        cdq
+        div     ebx
+
+        ; (AL) v = 16*(v / D)
+        ; --------------------------------------
+        push    ax
+        mov     eax, [calcuvd.u]
+        cdq
+        div     ebx
+        pop     bx
+
+        ; Расчет цвета
+        xor     al, bl
+        or      al, 0x10
+        mov     [.cl], al
+
+@@:     mov     al, [.cl]
+        mov     di, [.cursor]
+        stosb                   ; Рисование пикселя
+        mov     [.cursor], di
+        ;inc     [cursor]
+
+        ; Приращение u,v,D параметров
+        increm  calcuvd.u, data_com+0
+        increm  calcuvd.v, data_com+6
+        increm  calcuvd.D, data_com+12
+
+        ; Обработка одной строки
+        dec     cx
+        jne     .L1
+        dec     [calcuvd.dy]
+        pop     cx
+
+        ; Обработка всего экрана
+        dec     cx
+        jne     .L2
         ret
 
-; Скалярное произведение d*vec3
-.scalar_mul:
+.cursor dw      0
+.cl     db      0
 
-        movzx   eax, word [di]
+; ----------------------------------------------------------------------
+calcuvd:
+
+        call    .mult
+        mov     [.u], eax   ; u = dx*A1 + dy*A2 + dz*A3
+        call    .mult
+        mov     [.v], eax   ; v = dx*B1 + dy*B2 + dz*B3
+        call    .mult
+        mov     [.D], eax   ; D = dx*C1 + dy*C2 + dz*C3
+        ret
+
+.mult:  ; Скалярное произведение d*vec3
+        movsx   eax, word [di]          ; A.i
         mul     dword [.dx]
         xchg    eax, ebx
-        movzx   eax, word [di+2]
+        movsx   eax, word [di+2]        ; B.i
         mul     dword [.dy]
         add     ebx, eax
-        movzx   eax, word [di+4]
+        movsx   eax, word [di+4]        ; C.i
         mul     dword [.dz]
         add     eax, ebx
         add     di, 6
         ret
+
+.dx     dd      -160
+.dy     dd      100
+.dz     dd      100
+.u      dd      0
+.v      dd      0
+.D      dd      0
 
 ; ----------------------------------------------------------------------
 ; ВЫЧИСЛЕНИЕ ПАРАМЕТРОВ ТРЕУГОЛЬНИКА В ПРОСТРАНСТВЕ
@@ -125,11 +205,10 @@ create_parameter_tri:
 ; ----------------------------------------------------------------------
 data_tri:
 
-        dw      -5,  5, 15      ; Точка A
+        dw      -5,  5,  5      ; Точка A
         dw       5,  5,  6      ; Точка B
-        dw      -5, -5,  6      ; Точка C
+        dw      -5, -5,  7      ; Точка C
 
 ; Итоговые параметры треугольника (sizeof = 18 байт)
 ; ----------------------------------------------------------------------
 data_com:       times 9 dw ?
-
