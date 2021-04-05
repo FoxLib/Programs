609496219b38999a1ac0c97672be5443966544f1
FoxLib <hplus.foxtail@gmail.com>
2021-04-05 08:32:38 +0300

    Output to files

--- misc/image2scr/main.cc
+++ misc/image2scr/main.cc
@@ -11,12 +11,12 @@ int main(int argc, char** argv) {
 
     if (argc <= 1) { printf("Требуется имя файла\n"); return 1; }
     if (m.load_ppm(argv[1])) { printf("Должен быть файл 256x192\n"); return 1; }
-
+printf("%d ", argc);
     m.normalize();
     m.dither();
     m.clashing();
-    m.save_ppm("result_output.ppm");
-    m.save_scr("result_output.scr");
+    m.save_ppm(argc > 2 ? argv[2] : "result_output.ppm");
+    m.save_scr(argc > 3 ? argv[3] : "result_output.scr");
 
     return 0;
 }
