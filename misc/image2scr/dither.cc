d1121cb90f7276310930284aac3e1504d8e42937
FoxLib <hplus.foxtail@gmail.com>
2021-04-05 19:02:30 +0300

    New dither

--- misc/image2scr/dither.cc
+++ misc/image2scr/dither.cc
@@ -4,6 +4,7 @@
 #define MAX_Y 192
 
 struct RGB { float r, g, b; };
+struct SortColor { int id, value; };
 
 class DITHER {
 
@@ -14,9 +15,9 @@ protected:
     int  width, height, depth;
     char buf[256];
 
-    struct RGB src   [MAX_X][MAX_Y];
-    struct RGB source[MAX_X][MAX_Y];
-    int        dest  [MAX_X][MAX_Y];
+    struct RGB src      [MAX_X][MAX_Y];
+    struct RGB source   [MAX_X][MAX_Y];
+    int        dest  [2][MAX_X][MAX_Y];
 
     int area_bits [32][192];
     int area_attr [768];
@@ -54,13 +55,13 @@ public:
         depth = 8; // 16
 
         palette[0].r  = 0x00; palette[0].g = 0x00; palette[0].b = 0x00;  // 0 Черный
-        palette[1].r  = 0x00; palette[1].g = 0x00; palette[1].b = 0x80;  // 1 Синий
-        palette[2].r  = 0x80; palette[2].g = 0x00; palette[2].b = 0x00;  // 2 Красный
-        palette[3].r  = 0x80; palette[3].g = 0x00; palette[3].b = 0x80;  // 3 Фиолетовый
-        palette[4].r  = 0x00; palette[4].g = 0x80; palette[4].b = 0x00;  // 4 Зеленый
-        palette[5].r  = 0x00; palette[5].g = 0x80; palette[5].b = 0x80;  // 5 Голубой
-        palette[6].r  = 0x80; palette[6].g = 0x80; palette[6].b = 0x00;  // 6 Желтый
-        palette[7].r  = 0x80; palette[7].g = 0x80; palette[7].b = 0x80;  // 7 Серый
+        palette[1].r  = 0x00; palette[1].g = 0x00; palette[1].b = 0xc0;  // 1 Синий
+        palette[2].r  = 0xc0; palette[2].g = 0x00; palette[2].b = 0x00;  // 2 Красный
+        palette[3].r  = 0xc0; palette[3].g = 0x00; palette[3].b = 0xc0;  // 3 Фиолетовый
+        palette[4].r  = 0x00; palette[4].g = 0xc0; palette[4].b = 0x00;  // 4 Зеленый
+        palette[5].r  = 0x00; palette[5].g = 0xc0; palette[5].b = 0xc0;  // 5 Голубой
+        palette[6].r  = 0xc0; palette[6].g = 0xc0; palette[6].b = 0x00;  // 6 Желтый
+        palette[7].r  = 0xc0; palette[7].g = 0xc0; palette[7].b = 0xc0;  // 7 Серый
 
         palette[8 ].r = 0x00; palette[8 ].g = 0x00; palette[8 ].b = 0x00; // 0
         palette[9 ].r = 0x00; palette[9 ].g = 0x00; palette[9 ].b = 0xff; // 1
@@ -141,7 +142,17 @@ public:
     }
 
     // -----------------------------------------------------------------
-    void normalize(int gray) {
+
+    // Копировать точки во временное хранение
+    void copy_source() {
+
+        for (int y = 0; y < height; y++)
+        for (int x = 0; x < width;  x++)
+            source[x][y] = src[x][y];
+    }
+
+    // Приведение всех цветов к виду 0..192
+    void halftoned() {
 
         struct RGB max = {-1, -1, -1};
 
@@ -159,98 +170,105 @@ public:
 
         if (smax > 0) {
 
-            //float f = 128.0 / smax;
-            float f = 128.0 / smax;
+            float f = 192.0 / smax;
             for (int y = 0; y < height; y++)
             for (int x = 0; x < width; x++) {
 
                 src[x][y].r *= f;
                 src[x][y].g *= f;
                 src[x][y].b *= f;
-
-                if (gray) {
-
-                    float f = (src[x][y].r + src[x][y].g + src[x][y].b)/3;
-                    src[x][y].r = f;
-                    src[x][y].g = f;
-                    src[x][y].b = f;
-                }
-
             }
         }
     }
 
-    // Процедура дизеринга (0=nearest 1=floyd)
-    void dither_floyd(int type) {
+    // Перевести в Ч/Б
+    void grayscale() {
 
-        // Копировать точки во временное хранение
         for (int y = 0; y < height; y++)
-        for (int x = 0; x < width;  x++)
-            source[x][y] = src[x][y];
+        for (int x = 0; x < width; x++) {
+
+            float f = (source[x][y].r + source[x][y].g + source[x][y].b)/3;
+            source[x][y].r = f;
+            source[x][y].g = f;
+            source[x][y].b = f;
+        }
+    }
+
+    // -----------------------------------------------------------------
+
+    // Ближайший сосед
+    void dither_nearest(int dest_id) {
 
         // Выполнить дизеринг
         for (int y = 0; y < height; y++)
         for (int x = 0; x < width;  x++) {
 
-            struct RGB old = source[x][y];              // Старые цвета
-            int color_id   = search_nearest(old);       // Поиск ближайшего цвета из палитры
-            RGB ncl        = palette[color_id];         // Заменить на новый цвет (из палитры)
-            source[x][y]   = ncl;                       // Записать приблизительный цвет
-            dest[x][y]     = color_id;                  // Полученный индекс цвета
+            int color_id   = search_nearest(source[x][y]);  // Поиск ближайшего цвета из палитры
+            source[x][y]   = palette[color_id];             // Записать приблизительный цвет
+            dest[dest_id][x][y] = color_id;                 // Полученный индекс цвета
+        }
+    }
 
-            if (type) {
+    // Процедура дизеринга (0=nearest 1=floyd)
+    void dither_floyd(int dest_id) {
 
-                // Вычисляем ошибку квантования
-                RGB quant;
+        // Выполнить дизеринг
+        for (int y = 0; y < height; y++)
+        for (int x = 0; x < width;  x++) {
 
-                quant.r = (old.r - ncl.r);
-                quant.g = (old.g - ncl.g);
-                quant.b = (old.b - ncl.b);
+            struct RGB old = source[x][y];          // Старые цвета
+            int color_id   = search_nearest(old);   // Поиск ближайшего цвета из палитры
+            RGB ncl        = palette[color_id];     // Заменить на новый цвет (из палитры)
+            source[x][y]   = ncl;                   // Записать приблизительный цвет
+            dest[dest_id][x][y] = color_id;         // Полученный индекс цвета
 
-                //   x 7
-                // 3 5 1
+            // Вычисляем ошибку квантования
+            RGB quant;
 
-                // [+1, +0] 7/16
-                if (x + 1 < width) {
+            quant.r = (old.r - ncl.r);
+            quant.g = (old.g - ncl.g);
+            quant.b = (old.b - ncl.b);
 
-                    source[x + 1][y].r += (quant.r * 7.0/16.0);
-                    source[x + 1][y].g += (quant.g * 7.0/16.0);
-                    source[x + 1][y].b += (quant.b * 7.0/16.0);
-                }
+            //   x 7
+            // 3 5 1
 
-                // [-1, +1] 3/16
-                if (x - 1 >= 0 && y + 1 < height) {
+            // [+1, +0] 7/16
+            if (x + 1 < width) {
 
-                    source[x-1][y+1].r += (quant.r * 3.0/16.0);
-                    source[x-1][y+1].g += (quant.g * 3.0/16.0);
-                    source[x-1][y+1].b += (quant.b * 3.0/16.0);
-                }
+                source[x + 1][y].r += (quant.r * 7.0/16.0);
+                source[x + 1][y].g += (quant.g * 7.0/16.0);
+                source[x + 1][y].b += (quant.b * 7.0/16.0);
+            }
 
-                // [+0, +1] 5/16
-                if (y + 1 < height) {
+            // [-1, +1] 3/16
+            if (x - 1 >= 0 && y + 1 < height) {
 
-                    source[x][y+1].r += (quant.r * 5.0/16.0);
-                    source[x][y+1].g += (quant.g * 5.0/16.0);
-                    source[x][y+1].b += (quant.b * 5.0/16.0);
-                }
+                source[x-1][y+1].r += (quant.r * 3.0/16.0);
+                source[x-1][y+1].g += (quant.g * 3.0/16.0);
+                source[x-1][y+1].b += (quant.b * 3.0/16.0);
+            }
 
-                // [+1, +1] 1/16
-                if (x + 1 < width && y + 1 < height) {
+            // [+0, +1] 5/16
+            if (y + 1 < height) {
 
-                    source[x+1][y+1].r += (quant.r * 1.0/16.0);
-                    source[x+1][y+1].g += (quant.g * 1.0/16.0);
-                    source[x+1][y+1].b += (quant.b * 1.0/16.0);
-                }
+                source[x][y+1].r += (quant.r * 5.0/16.0);
+                source[x][y+1].g += (quant.g * 5.0/16.0);
+                source[x][y+1].b += (quant.b * 5.0/16.0);
+            }
+
+            // [+1, +1] 1/16
+            if (x + 1 < width && y + 1 < height) {
+
+                source[x+1][y+1].r += (quant.r * 1.0/16.0);
+                source[x+1][y+1].g += (quant.g * 1.0/16.0);
+                source[x+1][y+1].b += (quant.b * 1.0/16.0);
             }
         }
     }
 
-    /**
-     * Дизеринг с помощью матрицы Байера
-     */
+    // Дизеринг с помощью матрицы Байера
+    void dither_bayer(int dest_id) {
 
-    void ordered_dither()
-    {
         int ditherSize = 8;
 
         float lookup[8][8] =
@@ -265,11 +283,7 @@ public:
             {63, 31, 55, 23, 61, 29, 53, 21},
         };
 
-        for (int y = 0; y < height; y++)
-        for (int x = 0; x < width;  x++)
-            source[x][y] = src[x][y];
-
-        float f = 1.2, m = 32.0;
+        float f = 1.5, m = 32.0;
         for (int y = 0; y < height; y++)
         for (int x = 0; x < width;  x++) {
 
@@ -283,7 +297,96 @@ public:
             // Поиск ближайшего цвета из палитры
             int color_id = search_nearest(new_cl);
             source[x][y] = palette[color_id];
-            dest  [x][y] = color_id;
+            dest[dest_id][x][y] = color_id;
+        }
+    }
+
+    // -----------------------------------------------------------------
+
+    // Вторая версия
+    void clashing_v2() {
+
+        int i, j, k, x, y;
+        struct SortColor t[2][16];
+
+        for (y = 0; y < 24; y++)
+        for (x = 0; x < 32; x++) {
+
+            // Одни цвета для 1, другие для 0
+            for (i = 0; i < 2; i++)
+            for (j = 0; j < 16; j++)
+                t[i][j] = {0, 0};
+
+            // Подсчет цветов
+            for (i = 0; i < 8; i++) {
+
+                int chr = 0;
+                for (j = 0; j < 8; j++) {
+
+                    int fx = x*8 + j,
+                        fy = y*8 + i,
+                        cbit = dest[0][fx][fy] ? 1 : 0;
+
+                    // Если есть тон, это 1, иначе 0
+                    if (cbit) chr |= (128 >> j);
+
+                    // Цветное изображение
+                    int color_id = dest[1][fx][fy] & 15;
+
+                    t[ cbit ][ color_id ].id = color_id;
+                    t[ cbit ][ color_id ].value++;
+                }
+
+                // Записывается информация о цветах
+                area_bits[x][8*y+i] = chr;
+            }
+
+            // Выстроить самые часто встречаемые цвета
+            for (k = 0;   k < 2;  k++)
+            for (i = 0;   i < 16; i++)
+            for (j = i+1; j < 16; j++) {
+
+                if (t[k][i].value > t[k][j].value) {
+                    struct SortColor _temp  = t[k][i];
+                    t[k][i] = t[k][j];
+                    t[k][j] = _temp;
+                }
+            }
+
+            // Анализ цветов
+            int back = 0, fore = 7;
+
+            for (i = 0; i < 16; i++) { printf("%02d ", t[0][i].value); } printf("\n");
+            for (i = 0; i < 16; i++) { printf("%02d ", t[1][i].value); } printf("\n");
+            printf("\n----\n");
+
+            // Абсолютно однозначные цвета
+            if (t[0][14].value == 0)     back = t[0][15].id;
+            else if (t[0][14].value < 4) back = t[0][15].id;
+
+            if (t[1][14].value == 0)     fore = t[1][15].id;
+            else if (t[1][14].value < 4) fore = t[1][15].id;
+
+            // Серый цвет как 1, иначе 0
+            area_attr[32*y+x] = (back&7)<<3 | (fore&7);
+        }
+    }
+
+    // Конвертация изображения
+    void scr2source() {
+
+        for (int y = 0; y < 192; y++)
+        for (int x = 0; x < 32; x++) {
+
+            int at = area_attr[32*(y>>3)+x];
+            int ch = area_bits[x][y];
+
+            RGB fr = palette[at & 7];
+            RGB bk = palette[(at>>3) & 7];
+
+            for (int i = 0; i < 8; i++) {
+                source[8*x+i][y] = (ch << i) & 128 ? fr : bk;
+            }
         }
     }
 
@@ -307,7 +410,7 @@ public:
                 // Подсчет цветов
                 for (int i = 0; i < 8; i++)
                 for (int j = 0; j < 8; j++)
-                    color_table[ dest[x*8+j][y*8+i] & 15 ]++;
+                    color_table[ dest[1][x*8+j][y*8+i] & 15 ]++;
 
                 // Самый популярный цвет
                 for (int i = 0; i < 16; i++)
--- misc/image2scr/main.cc
+++ misc/image2scr/main.cc
@@ -13,11 +13,36 @@ int main(int argc, char** argv) {
     if (argc <= 1) { printf("Требуется имя файла\n"); return 1; }
     if (m.load_ppm(argv[1])) { printf("Должен быть файл 256x192\n"); return 1; }
 
-    m.normalize(1);
-    //m.dither_floyd(1);
-    m.ordered_dither();
+    // Нормализация яркости
+    m.halftoned();
+
+/*
+    m.copy_source();
+    m.grayscale();
+
+    // Метод дизеринга
+    // --------------------
+    //m.dither_nearest(0);
+    //m.dither_floyd(0);
+    m.dither_bayer(0);          // В dest[0] находится черно-белое изображение
+    // --------------------
+*/
+    // Метод дизеринга
+    // --------------------
+    m.copy_source();
+    //m.dither_bayer(1);          // В dest[1] находится цветное
+    m.dither_floyd(1);          // В dest[1] находится цветное
+    //m.dither_nearest(1);          // В dest[1] находится цветное
+    // --------------------
+
+    // Клешинг
+    //m.clashing_v2();
     m.clashing();
 
+    // Обновление
+    m.scr2source();
+
+    // Вывод данных
     m.save_ppm(argc > 2 ? argv[2] : "result_output.ppm");
     m.save_scr(argc > 3 ? argv[3] : "result_output.scr");
 
