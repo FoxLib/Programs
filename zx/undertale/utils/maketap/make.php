796fcd82d2fc6f4ec4ee65f7befb42db4155abf0
FoxLib <hplus.foxtail@gmail.com>
2021-04-23 07:48:45 +0300

    Загрузка кода

--- zx/undertale/utils/maketap/make.php
+++ zx/undertale/utils/maketap/make.php
@@ -11,8 +11,7 @@ function crc8($block, $crconly = false) {
 $data = file_get_contents("ut.tap");
 $scr  = file_get_contents("../../screen/src/screen.scr");
 
-
-// Модицикация строки автостарта
+// Модификация строки автостарта
 // ---------------------------------------------------------------------
 $data[0x10] = chr(0x01);
 $data[0x11] = chr(0x00);
@@ -20,7 +19,6 @@ $data[0x11] = chr(0x00);
 // Пересчет контрольной суммы
 $x = array_map('ord', str_split(substr($data, 2, 18)));
 $data[0x14] = chr(crc8($x, true));
-
 // ---------------------------------------------------------------------
 
 // Первым идет экран Undertale
@@ -29,12 +27,27 @@ $block = crc8($block);
 $screen = pack("C*", 0x13,0x00, ...$block);
 
 // Далее идет блок с данными
-$block = [0xFF]; for ($i = 0; $i < 6912; $i++) $block[] = ord($scr[$i]);
-$block = crc8($block);
+$block = [0xFF]; for ($i = 0; $i < 6912; $i++) $block[] = ord($scr[$i]); $block = crc8($block);
 $screen .= pack("C*", 0x02,0x1B, ...$block);
 
+// Блок с кодом
 // ---------------------------------------------------------------------
 
+$code = file_get_contents("../../undertale.bin");
+$size = strlen($code);
+
+// Первым идет блок с кодом
+$block = [0x00, 0x03, 0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20, ($size&255),($size>>8), 0x80,0x65, 0x00,0x80 ];
+$block = crc8($block);
+$codeblock = pack("C*", 0x13,0x00, ...$block);
+
+// Далее идет блок с данными
+$block = [0xFF]; for ($i = 0; $i < $size; $i++) $block[] = ord($code[$i]); $block = crc8($block);
+
+$size += 2;
+$codeblock .= pack("C*", ($size&255),($size>>8), ...$block);
+
 // Сохранить новый TAP файл
-file_put_contents("ut.tap", $data . $screen);
+// ---------------------------------------------------------------------
+file_put_contents("ut.tap", $data . $screen . $codeblock);
 
