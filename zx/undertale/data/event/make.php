21f8d2d8fa6a3b609cb1427b8271f1d0ee105184
FoxLib <hplus.foxtail@gmail.com>
2021-04-23 15:03:03 +0300

    Несколько экранов готово

--- zx/undertale/data/event/make.php
+++ zx/undertale/data/event/make.php
@@ -1,3 +1,88 @@
 <?php
 
-$event = "";
+$arr = [];
+
+// Первый экран
+$arr[] = "4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4LF";
+$arr[] = "4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4LF";
+$arr[] = "4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 85T ";
+$arr[] = "1R1 1R2 1R3 1R4 1R5 1R6 1R7 1R8 0K 1N 1R7 1R6 1R5 1R4 1R3 1R2 1R1 1R0";
+
+// Второй экран
+$arr[] = "4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4LF";
+$arr[] = "4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 85T";
+$arr[] = "0K 1R1 1R2 1R3 1R4 1R5 1R6 1R7 0K 1N 2R8 1R7 1R6 1R5 1R4 1R3 1R2 1R1 1R0";
+
+// Третий экран
+$arr[] = "4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4LF";
+$arr[] = "4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4LF";
+$arr[] = "4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 85LF";
+$arr[] = "0K 1R1 1R2 1R3 1R4 1R5 1R6 1R7 0K  2R8 1R7 1R6 1R5 1R4 1R3 1R2 1R1 1R0";
+
+// Четвертый экран
+$arr[] = "4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4LF";
+$arr[] = "4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4LF";
+$arr[] = "4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 4T 140LF";
+$arr[] = "0K 1R1 1R2 1R3 1R4 1R5 1R6 1R7 0K 1N 2R8 1R7 1R6 1R5 1R4 1R3 1R2 1R1 1R0";
+
+
+// ---------------------------------------------------------------------
+
+$output = [];
+foreach ($arr as $items) {
+
+    $items = explode(' ', $items);
+    foreach ($items as $item) {
+
+        // Пропечатка буквы
+        if (preg_match('~(\d+)T~', $item, $c)) {
+            $output[] = $c[1];
+            $output[] = 1;
+        }
+        // Дизеринг и перерисовка
+        else if (preg_match('~(\d+)R(\d+)~', $item, $c)) {
+            $output[] = $c[1];
+            $output[] = 2;
+            $output[] = $c[2];
+        }
+        // Следующий экран
+        else if (preg_match('~(\d+)N~', $item, $c)) {
+            $output[] = $c[1];
+            $output[] = 3;
+        }
+        // Перенос строки
+        else if (preg_match('~(\d+)LF~', $item, $c)) {
+            $output[] = $c[1];
+            $output[] = 4;
+        }
+        // Стереть буквы
+        else if (preg_match('~(\d+)K~', $item, $c)) {
+            $output[] = $c[1];
+            $output[] = 5;
+        }
+        // Ожидание
+        else if (preg_match('~(\d+)W~', $item, $c)) {
+            $output[] = $c[1];
+            $output[] = 0;
+        }
+    }
+}
+
+// Финализация
+$output[] = 0xff; $output[] = 0xff;
+
+// Формирование вывода
+$lines = [];
+$cols = [];
+foreach ($output as $k => $val) {
+
+    $cols[] = sprintf("$%02x", $val);
+    if (($k & 15) == 15) {
+        $lines[] = "defb " . join(",", $cols);
+        $cols = [];
+    }
+}
+if ($cols) $lines[] = "defb " . join(",", $cols);
+
+file_put_contents("../events.asm", join("\n", $lines));
+
--- zx/undertale/main.asm
+++ zx/undertale/main.asm
@@ -1,101 +1,101 @@
 
-        org     $6600       ; Начало после Бейсик-программы
-DECOMP: equ     $5b00       ; Буфер A28h (2600) байт для распаковки экрана
+            org     $6600       ; Начало после Бейсик-программы
+DECOMP:     equ     $5b00       ; Буфер A28h (2600) байт для распаковки экрана
 
-        di
+            di
 
-        ; Очистка экрана
+            ; Очистка экрана
 ; ----------------------------------------------------------------------
-        ld      sp, $5b00
-        ld      hl, $4747       ; Очистка области атрибутов
-        ld      bc, $8002
-CLS1:   push    hl
-        djnz    CLS1
-        dec     c
-        jr      nz, CLS1
-        ld      hl, $0000       ; Очистка области данных
-        ld      bc, $000c
-CLS2:   push    hl
-        djnz    CLS2
-        dec     c
-        jr      nz, CLS2
+            ld      sp, $5b00
+            ld      hl, $4747       ; Очистка области атрибутов
+            ld      bc, $8002
+CLS1:       push    hl
+            djnz    CLS1
+            dec     c
+            jr      nz, CLS1
+            ld      hl, $0000       ; Очистка области данных
+            ld      bc, $000c
+CLS2:       push    hl
+            djnz    CLS2
+            dec     c
+            jr      nz, CLS2
 ; ----------------------------------------------------------------------
 
-        ; Установка прерывания IM2
-        ld      a, $65
-        ld      i, a
-        ld      a, r
-        and     $7f
-        ld      r, a            ; Сброс бита R[7] если он там был
-        ld      hl, IM2
-        ld      sp, $65ff
-        ex      (sp), hl
-        im      2
+            ; Установка прерывания IM2
+            ld      a, $65
+            ld      i, a
+            ld      a, r
+            and     $7f
+            ld      r, a            ; Сброс бита R[7] если он там был
+            ld      hl, IM2
+            ld      sp, $65ff
+            ex      (sp), hl
+            im      2
 
-        ; Первичная отрисовка
-        call    AYINIT
-        call    NEXTSCR         ; Загрузить следующий экран
-        xor     a
-        call    DRAWBG          ; Чтобы затереть белые атрибуты
-        ld      a, (DithV)
-        ld      hl, DECOMP
-        call    REDRAW          ; "Невидимое" рисование
-        ld      a, $30
-        call    DRAWBG          ; Сверхскоростное появление на экране
+            ; Первичная отрисовка
+            call    AYINIT
+            call    NEXTSCR         ; Загрузить следующий экран
+            xor     a
+            call    DRAWBG          ; Чтобы затереть белые атрибуты
+            ld      a, (DithV)
+            ld      hl, DECOMP
+            call    REDRAW          ; "Невидимое" рисование
+            ld      a, $30
+            call    DRAWBG          ; Сверхскоростное появление на экране
 
-        ; Ожидание появления события
-        ; -------------------------------
-MAIN:   ei
-        ld      a, r
-        and     $80
-        jr      z, MAIN
-        ld      a, r
-        and     $7f
-        ld      r, a
+            ; Ожидание появления события
+            ; -------------------------------
+MAIN:       ei
+            ld      a, r
+            and     $80
+            jr      z, MAIN
+            ld      a, r
+            and     $7f
+            ld      r, a
 
-        ; -------------------------------
-        ; Обновление, загрузка данных
-        ; -------------------------------
+            ; -------------------------------
+            ; Обновление, загрузка данных
+            ; -------------------------------
 
-        ld      a, (DithV)
-        cp      $ff
-        jr      nz, DRAWS
-        call    NEXTSCR         ; Декомпрессия нового экрана
-        jr      MAIN
-DRAWS:  ld      hl, DECOMP
-        call    REDRAW
-        jr      MAIN
+            ld      a, (DithV)
+            cp      $ff
+            jr      nz, DRAWS
+            call    NEXTSCR         ; Декомпрессия нового экрана
+            jr      MAIN
+DRAWS:      ld      hl, DECOMP
+            call    REDRAW
+            jr      MAIN
 
 ; ----------------------------------------------------------------------
 ; Обработчики и данные
 ; ----------------------------------------------------------------------
 
-        ; Обработчики
-        include "routines.asm"
+            ; Обработчики
+            include "routines.asm"
 
-        ; Декомпрессор
-        include "dzx0_standard.asm"
+            ; Декомпрессор
+            include "dzx0_standard.asm"
 
-        ; Список строк
-STRG:   include "data/string.asm"
+            ; Список строк
+STRG:       include "data/string.asm"
 
-        ; Список событий
-EVTLST: include "events.asm"
+            ; Список событий
+EVTLST:     include "data/events.asm"
 
-YTABLE: incbin  "data/ytable.bin"
-DMASK0: incbin  "data/dither.bin"
-FONTS:  incbin  "data/fonts.bin"
+YTABLE:     incbin  "data/ytable.bin"
+DMASK0:     incbin  "data/dither.bin"
+FONTS:      incbin  "data/fonts.bin"
 
-        ; Компрессированные данные со скринами (около 12 кб)
-SCR1:   incbin  "screen/screen1.bin"
-SCR2:   incbin  "screen/screen2.bin"
-SCR3:   incbin  "screen/screen3.bin"
-SCR4:   incbin  "screen/screen4.bin"
-SCR5:   incbin  "screen/screen5.bin"
-SCR6:   incbin  "screen/screen6.bin"
-SCR7:   incbin  "screen/screen7.bin"
-SCR8:   incbin  "screen/screen8.bin"
-SCR9:   incbin  "screen/screen9.bin"
+            ; Компрессированные данные со скринами (около 12 кб)
+SCR1:       incbin  "screen/screen1.bin"
+SCR2:       incbin  "screen/screen2.bin"
+SCR3:       incbin  "screen/screen3.bin"
+SCR4:       incbin  "screen/screen4.bin"
+SCR5:       incbin  "screen/screen5.bin"
+SCR6:       incbin  "screen/screen6.bin"
+SCR7:       incbin  "screen/screen7.bin"
+SCR8:       incbin  "screen/screen8.bin"
+SCR9:       incbin  "screen/screen9.bin"
 
 ; ----------------------------------------------------------------------
 ; Данные
@@ -105,12 +105,27 @@ IYhold:     defw    0           ; Временное значение для IY 
 DithV:      defb    0           ; Текущее значение затемнения (от 0 до 8)
 EVAddr:     defw    EVTLST      ; Адрес последнего байта события в потоке
 
-; Текущее событие
-KEvent:     defb    0           ; Время (1/50 сек) до следующего события
-
 ; Таблица с экранами
 SCRPTR:     defw    SCRLST
 SCRLST:     defw    SCR1, SCR2, SCR3, SCR4, SCR5, SCR6, SCR7, SCR8, SCR9
 NEXTSYM:    defw    str0        ; Адрес печатаемого символа
 NEXTPOS:    defw    $5004       ; Первая позиция текста
 
+; ----------------------------------------------------------------------
+; Типы событий
+; ----------------------------------------------------------------------
+; 1 DELAY   Когда должно наступить следующее событие. Если =0, то прямо сейчас
+; 1 TYPE    Тип события
+; x PARAM   Параметры события
+; ----------------------------------------------------------------------
+; 00 Ничего не делать
+; 01 Печать следующей буквы
+; 02 Перерисовка экрана с новыми дизерингом (параметр)
+; 03 К следующему экрану
+; 04 CRLF
+; 05 Очистка текста
+; xx Окончание потока
+; ----------------------------------------------------------------------
+
+; Текущее событие
+KEvent:     defb    0           ; Время (1/50 сек) до следующего события
