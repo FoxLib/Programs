a9c2b7a5832c5a0f88c7dcbda88bee4f0e0bdaa4
FoxLib <hplus.foxtail@gmail.com>
2021-05-29 21:52:04 +0300

    Небольшие оптимизации

--- asm/06_bootzx/main.asm
+++ asm/06_bootzx/main.asm
@@ -18,20 +18,20 @@
             mov     ax, $0800
             mov     ds, ax              ; DS=8000h
 
-            call    render
-            jmp     $
-
+brk
             mov     bp, 0x4000
-            mov     si, program
+            ;xor     si, si              ; pc=0
+            mov     si, 1
+            call    savef
 next:       lodsb
-            mov     bx, [bp+4]      ; bx=hl
+            mov     bx, [bp+4]          ; bx=hl
             mov     di, pattern
-@@:         mov     dx, [di]
+@@:         mov     dx, [cs:di]
             add     di, 4
             and     dh, al
             cmp     dl, dh
             jne     @b
-            call    word [di-2]
+            call    word [cs:di-2]
             jmp     next
 
             ; LD r8, r8
@@ -49,10 +49,14 @@ ldi8:       mov     ah, [si]
             call    setreg8di
             ret
 
-            ; <alu> a,r8
 ; ----------------------------------------------------------------------
-alues:      call    getreg8lo
-            call    get53di
+            ; <alu> a,imm
+aluimm:     mov     ah, [si]
+            inc     si
+            jmp     alues.exec
+            ; <alu> a,r8
+alues:      call    getreg8lo       ; AH=значение регистра
+.exec:      call    get53di         ; DI=номер операции АЛУ
             call    aluop
             ret
 
@@ -60,13 +64,14 @@ alues:      call    getreg8lo
 ; ----------------------------------------------------------------------
 aluop:      mov     al, [cs:di+replacealu]
             mov     [cs:codealu], al
-            call    savef
-codealu:    add     [bp+7], ah  ; Модифируемый код
             call    loadf
+codealu:    add     [bp+7], ah      ; Модифируемый код
+            call    savef
             ret
 
 ; Трансляция для 80 -> 86
-replacealu: db 0x00, 0x10, 0x28, 0x20, 0x30, 0x08, 0x18, 0x38
+;               ADD   ADC   SUB   SBC   AND   XOR    OR    CP
+replacealu: db 0x00, 0x10, 0x28, 0x18, 0x20, 0x30, 0x08, 0x38
 
 ; Прочесть регистр DI => AH
 ; ----------------------------------------------------------------------
@@ -85,7 +90,7 @@ get53di:    mov     di, ax
             and     di, 7
             ret
 
-; Сохранить AH в регистр DI
+; Сохранить AH в регистр номер DI
 ; ----------------------------------------------------------------------
 setreg8di:  cmp     di, 6
             mov     [bp+di], ah
@@ -116,9 +121,8 @@ render:     push    si
             mov     al, [bx]        ; BH=передний цвет, BL=задний
             mov     bl, al
             mov     bh, al
-            and     bh, 7           ; BH=attr&7
             shr     bl, 3
-            and     bl, 7           ; BL=(attr>>3)&7
+            and     bx, $0707       ; BH=attr&7, BL=(attr>>3)&7
             test    al, $40         ; BRIGHT
             je      .R2
             or      bx, $0808
@@ -172,12 +176,10 @@ DAP_sector: ; BIOS DAP https://en.wikipedia.org/wiki/INT_13H
 ; ----------------------------------------------------------------------
 pattern:
 
+    instr   11000111b, 11000110b, aluimm
     instr   11000111b, 00000110b, ldi8
     instr   11000000b, 10000000b, alues
     instr   11000000b, 01000000b, moves
     instr   00000000b, 00000000b, error
 
 error:
-
-; Выделить сегмент
-program:    db      0x8A
